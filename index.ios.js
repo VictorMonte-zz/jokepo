f7640ab4c4cd56561fa84747dfc448d72b6bdff1
victor.monte <victorbcm@hotmail.com>
2019-05-13 21:39:00 -0300

    Add random computer choice

--- index.ios.js
+++ index.ios.js
@@ -15,18 +15,28 @@ class jokepo extends Component {
     }
 
     jokenpo(escolhaUsuario) {
-        this.setState({ escolhaUsuario: escolhaUsuario });
+        var numeroAleatorio = Math.floor(Math.random() * 3);
+        var escolhaComputador = '';
+
+        switch (numeroAleatorio) {
+            case 0: escolhaComputador = 'pedra'; break;
+            case 1: escolhaComputador = 'papel'; break;
+            case 2: escolhaComputador = 'tesoura'; break;
+            default: break;
+        }
+
+        this.setState({ escolhaUsuario, escolhaComputador });
     }
 
     render() {
         return (
            <View style={{ marginTop: 100 }}>
-                <Text>Escolha do computador</Text>
+                <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
                 <Text>Escolha do usuario: {this.state.escolhaUsuario}</Text>
                 <Text>Resultado</Text>
                 <Button title="pedra" onPress={() => this.jokenpo('pedra')} />
-                <Button title="papel" onPress={() => this.jokenpo('papel')}/>
-                <Button title="tesoura" onPress={() => this.jokenpo('tesoura')}/>
+                <Button title="papel" onPress={() => this.jokenpo('papel')} />
+                <Button title="tesoura" onPress={() => this.jokenpo('tesoura')} />
            </View>
         );
     }
